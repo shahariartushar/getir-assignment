92b40a94c5d103766ace90e7fd1461a41803a743
shahariartushar <shahariartushar@gmail.com>
2022-01-28 01:33:51 +0100

    refactor(startup/db): code refactor

--- startup/db.js
+++ startup/db.js
@@ -1,8 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-
 module.exports = function () {
-    mongoose
-    .connect(mongoose.connect(config.get('db')), { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log(`Connected to ${dbUri}...`)).catch((e) => {throw new Error(e)});
+mongoose.connect(config.get('db'),{ useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log(`Connected to Database`));
 };
